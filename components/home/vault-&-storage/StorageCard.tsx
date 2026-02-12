cb40e850dabce48200f70245a2527f58e1f756d4
sheikhharis50 <itshariszahid@gmail.com>
2026-02-12 16:26:11 +0500

    feat: Vault and storage section

--- components/home/vault-&-storage/StorageCard.tsx
+++ components/home/vault-&-storage/StorageCard.tsx
@@ -1,17 +1,21 @@
-import SliderCardPill from "@/components/SliderCardPill";
-import ThunderCard from "@/components/ThunderCardBg";
-import PC from "@/public/images/home/vault/Storage-Pc.png";
 import Image from "next/image";
+import ThunderCard from "@/components/ThunderCardBg";
+import SliderCardPill from "@/components/SliderCardPill";
+import pc from "@/public/images/home/vault/storage.png";
 
 const StorageCard = () => {
   return (
-    <ThunderCard>
-      <SliderCardPill text="Storage" className="mt-5 ml-8" />
-      <Image
-        src={PC}
-        alt="bg"
-        className="w-140 h-auto opacity-95 pl-10 pt-12 mb-4"
-      />
+    <ThunderCard className="h-full py-5 sm:py-6 pl-3 xs:pl-5 sm:pl-6 pr-5 sm:pr-14">
+      <div className="flex flex-col justify-between h-full">
+        <SliderCardPill text="Storage" className="mb-5" />
+        <Image
+          src={pc}
+          width={1000}
+          height={1500}
+          alt="computer"
+          className="w-63 xs:w-85 sm:w-90 xl:w-120 h-auto"
+        />
+      </div>
     </ThunderCard>
   );
 };
