39dc46c65d33128cb907a20d12a8a4ac49e48d50
sheikhharis50 <itshariszahid@gmail.com>
2026-02-16 16:52:39 +0500

    feat: Watch demo screen header

--- app/watch-demo/page.tsx
+++ app/watch-demo/page.tsx
@@ -1,15 +1,17 @@
 import Encryption from "@/components/watch-demo/encryption";
 import FAQ from "@/components/watch-demo/faq";
+import HeaderWatchDemo from "@/components/watch-demo/Header";
 import TechnicalArchitecture from "@/components/watch-demo/technicalArchitecture";
 import Transparency from "@/components/watch-demo/transparency";
 
 export default function Page() {
   return (
-    <div>
+    <>
+      <HeaderWatchDemo />
       <Encryption />
       <TechnicalArchitecture />
       <Transparency />
       <FAQ />
-    </div>
+    </>
   );
 }
