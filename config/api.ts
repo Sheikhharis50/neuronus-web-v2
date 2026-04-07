e71c898a75a9609882c4bc3a6bc71b6a11319ff3
Sheikh Haris Zahid <sheikh.haris.zahid@hotmail.com>
2026-04-07 15:02:11 +0500

    fix: bring back the use server in blog.ts

--- config/api.ts
+++ config/api.ts
@@ -1,3 +1,3 @@
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || process.env.API_BASE_URL;
 export const API_URL = `${API_BASE_URL}/api`;
-export const API_TOKEN = process.env.NEXT_PUBLIC_API_TOKEN;
+export const API_TOKEN = process.env.NEXT_PUBLIC_API_TOKEN || process.env.API_TOKEN;
